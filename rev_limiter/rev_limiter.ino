de642bf7cc0f69f2708885c237406ee72fc7a052
thomasneverland <thomas.neverland@gmail.com>
2024-02-18 12:31:10 +0100

    improved rpm reading logic

--- rev_limiter/rev_limiter.ino
+++ rev_limiter/rev_limiter.ino
@@ -1,36 +1,37 @@
 #define relay 5
 
-float sensorInput = (analogRead(A0) / 1024) * 5;
+int sensorInput = analogRead(A1);
 int RPM = 0;
-int CutRPM = 3000;
+int CutRPM = 2500;
 unsigned long previousMillis = 0UL;
-unsigned long interval = 1000UL; //miliseconds
+unsigned long interval = 300UL; //miliseconds
 
 void setup() {
   // put your setup code here, to run once:
   pinMode(relay, OUTPUT);
+  digitalWrite(relay, HIGH);
   Serial.begin(9600);
 }
 
 void loop() {
   unsigned long currentMillis = millis();
-
+  int raw = analogRead(A1);
+  float value = raw * (5.0 / 1024.0);
+  int RPM = value * 800 / 1.1;
+  Serial.println(RPM);
   if(currentMillis - previousMillis > interval)
   {
 	  //The Arduino executes this code once every second
  	  //(interval = 1000 (ms) = 1 second).
     if (RPM > CutRPM) {
-    digitalWrite(relay, HIGH);
-    Serial.println("Ignition Cut: OFF");
-    RPM = 0;
+      digitalWrite(relay, LOW);
+      Serial.println("Ignition Cut: ON");
     } else {
-    digitalWrite(relay, LOW);
-    Serial.println("Ignition Cut: ON");
-    RPM = 4000;
+      digitalWrite(relay, HIGH);
+      Serial.println("Ignition Cut: OFF");
     }
-    Serial.println(sensorInput);
     //Don't forget to update the previousMillis value
- 	  previousMillis = currentMillis;
+    previousMillis = currentMillis;
   }
 }
 
