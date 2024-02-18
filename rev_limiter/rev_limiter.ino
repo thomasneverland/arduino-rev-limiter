410067375ae90ecbd2271ff79b5480e343fb7ca9
thomasneverland <thomas.neverland@gmail.com>
2024-02-18 11:24:20 +0100

    Update rev_limiter.ino

--- rev_limiter/rev_limiter.ino
+++ rev_limiter/rev_limiter.ino
@@ -1,27 +1,41 @@
 #define relay 5
 
-int sensorInput = analogRead(A0);
-float voltage = sensorInput * (5.0 / 1023.0);
+float sensorInput = (analogRead(A0) / 1024) * 5;
 int RPM = 0;
 int CutRPM = 3000;
-int duration = 5000; //miliseconds
+unsigned long previousMillis = 0UL;
+unsigned long interval = 1000UL; //miliseconds
 
 void setup() {
+
   // put your setup code here, to run once:
   pinMode(relay, OUTPUT);
   Serial.begin(9600);
+
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-  if (RPM > CutRPM) {
+
+  unsigned long currentMillis = millis();
+
+  if(currentMillis - previousMillis > interval)
+  {
+	/* The Arduino executes this code once every second
+ 	*  (interval = 1000 (ms) = 1 second).
+ 	*/
+    if (RPM > CutRPM) {
     digitalWrite(relay, HIGH);
     Serial.println("Ignition Cut: OFF");
     RPM = 0;
-  } else {
+    } else {
     digitalWrite(relay, LOW);
     Serial.println("Ignition Cut: ON");
     RPM = 4000;
+    }
+    Serial.println(sensorInput);
+ 	// Don't forget to update the previousMillis value
+ 	  previousMillis = currentMillis;
   }
-  delay(duration);
+
 }
+
